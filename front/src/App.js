44f6dab9a3164d31c9a9e1895c65800b16b9ab54
Vitaliy Radchenko <rad4enko.vitaliy@gmail.com>
2023-12-18 21:02:00 +0200

    useRef

--- front/src/App.js
+++ front/src/App.js
@@ -1,38 +1,138 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Grid from "./component/grid";
 import Page from "./component/page";
-import PostList from "./container/post-list";
+import Box from "./component/box";
+// import PostList from "./container/post-list";
 
 function App() {
-  const [location, setLocation] = useState(null);
-
+  //4.
+  const scrollPositionRef = useRef(0);
+  const handleScroll = () => {
+    console.log(scrollPositionRef);
+    scrollPositionRef.current = window.scrollY;
+  };
   useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setLocation({ latitude, longitude });
-        },
-        (error) => {
-          console.error("Помилка отримання геолокаціїї", error.message);
-        }
-      );
-    } else {
-      console.error("Геолокація не підтримується в цьому браузері");
-    }
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+
+  //3.
+
+  // const prevValueRef = useRef("null");
+  // const [value, setValue] = useState(0);
+
+  // useEffect(() => {
+  //   console.log(value, "value");
+  //   console.log(prevValueRef, "prevValueRef");
+
+  //   prevValueRef.current = value;
+  // }, [value]);
+
+  // const handleIncrement = () => {
+  //   setValue(value + 1);
+  // };
+  // const handleDecrement = () => {
+  //   setValue(value - 1);
+  // };
+  //   console.log("render");
+
+  //2. Приклад фокусу на певному інпуті
+
+  // const inputRef = useRef(null);
+  // useEffect(() => {
+  //   if (inputRef && inputRef.current) inputRef.current.focus(); //Фокус на введенны після завантаження компонента
+  // }, []);
+
+  //1.Приклад привязки кнопки до картинки
+
+  // const firstCatRef = useRef(null);
+  // const secondCatRef = useRef(null);
+  // const thirdCatRef = useRef(null);
+
+  // function handleScrollBy(ref) {
+  //   console.log(ref);
+  //   if (ref && ref.current) {
+  //     const offsetTop = ref.current.offsetTop;
+  //     window.scrollBy({
+  //       top: offsetTop,
+  //       behavior: "smooth",
+  //     });
+  //   }
+  // }
+
   return (
+    //4.
+
     <Page>
-      {location ? (
-        <div>
-          <h2>Ваша геолокація:</h2>
-          <p>Широта: {location.latitude}</p>
-          <p>Довгота: {location.longitude}</p>
-        </div>
-      ) : (
-        <p>Отримання геолокаціїї... </p>
-      )}
-      <PostList />
+      <button onClick={handleScroll}>Click</button>
+      <p style={{ height: 10000 }}></p>
     </Page>
+
+    //3.
+
+    // <Page>
+    //   <Grid>
+    //     <Box>
+    //       <p>Current value: {value}</p>
+    //       <p>Previous Value: {prevValueRef.current}</p>
+    //     </Box>
+    //     <Box>
+    //       <button onClick={handleIncrement}>Increment</button>
+    //     </Box>
+    //     <Box>
+    //       <button onClick={handleDecrement}>Decrement</button>
+    //     </Box>
+    //   </Grid>
+    // </Page>
+
+    //2. Приклад фокусу на певному інпуті
+
+    // <Page>
+    //   <Grid>
+    //     <Box>
+    //       <input ref={inputRef} placeholder="Введіть пошту..." />
+    //     </Box>
+    //     <Box>
+    //       <input placeholder="Введіть пароль..." />
+    //     </Box>
+    //   </Grid>
+    // </Page>
+
+    //1. Приклад привязки кнопки до картинки
+    // <Page>
+    //   <Grid>
+    //     <button onClick={() => handleScrollBy(firstCatRef)}>Tom</button>
+    //     <button onClick={() => handleScrollBy(secondCatRef)}>Maru</button>
+    //     <button onClick={() => handleScrollBy(thirdCatRef)}>Jellylorum</button>
+    //   </Grid>
+    //   <div>
+    //     <ul style={{ display: "grid", gap: "500px", marginBottom: "500px" }}>
+    //       <li>
+    //         <img
+    //           src="https://placekitten.com/g/200/200"
+    //           alt="Tom"
+    //           ref={firstCatRef}
+    //         />
+    //       </li>
+    //       <li>
+    //         <img
+    //           src="https://placekitten.com/g/300/200"
+    //           alt="Maru"
+    //           ref={secondCatRef}
+    //         />
+    //       </li>
+    //       <li>
+    //         <img
+    //           src="https://placekitten.com/g/250/200"
+    //           alt="Jellylorum"
+    //           ref={thirdCatRef}
+    //         />
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </Page>
   );
 }
 
